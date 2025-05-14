794411671342a6c1b28ed490cce3487581982dc4
Joao Leviski Alves <joaol.alves@gmail.com>
2025-05-14 16:37:52 -0300

    custom hooks staff and treatments

--- base-lazy-days/client/src/components/staff/AllStaff.tsx
+++ base-lazy-days/client/src/components/staff/AllStaff.tsx
@@ -5,7 +5,6 @@ import { useStaff } from "./hooks/useStaff";
 import { Staff } from "./Staff";
 
 export function AllStaff() {
-  // replace with data from React Query
   const { staff, filter, setFilter } = useStaff();
   const treatments = useTreatments();
 
@@ -15,7 +14,7 @@ export function AllStaff() {
         Our Staff
       </Heading>
       <HStack m={10} spacing={8} justify="center">
-        {staff.map((staffData) => (
+        {staff?.map((staffData) => (
           <Staff key={staffData.id} staffData={staffData} />
         ))}
       </HStack>
@@ -23,7 +22,7 @@ export function AllStaff() {
         <HStack my={10} spacing={8} justify="center">
           <Heading size="md">Filter by treatment:</Heading>
           <Radio value="all">All</Radio>
-          {treatments.map((t) => (
+          {treatments?.map((t) => (
             <Radio key={t.id} value={t.name}>
               {t.name}
             </Radio>
